b6f0e530c121dde497351600f03a1c58593e811a
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-31 11:40:11 +0530

    ADD user registration to master

---
+++ src/userRegistration/UserRegistration.java
@@ -0,0 +1,10 @@
+package userRegistration;
+
+public class UserRegistration 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Validation of user entries in UserRegistration");
+	}
+	
+}
