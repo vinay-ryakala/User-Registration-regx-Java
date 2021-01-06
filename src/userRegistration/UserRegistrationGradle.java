818886edd52ed2b01e3fa677a7bbbb129d833675
vinay-ryakala <vinayrakyala@gmail.com>
2021-01-06 13:32:38 +0530

    ADDED: to throw custom exception

--- src/userRegistration/UserRegistrationGradle.java
+++ src/userRegistration/UserRegistrationGradle.java
@@ -1,36 +1,50 @@
-package userRegistration;
+package com.gradle;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationGradle
 {
-    boolean checkName(String name)
+    boolean checkName(String name)throws InvalidInputException
     {
+
         Pattern nameRegex = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,12}$");
         Matcher nameMatch = nameRegex.matcher(name);
-        return nameMatch.matches();
-    }
-    boolean mobileNumber(String number)
-    {
-        Pattern numberRegex = Pattern.compile("^[0-9]{2}\s[1-9]{1}[0-9]{9}$");
-        Matcher numberMatch = numberRegex.matcher(number);
-        return numberMatch.matches();
-    }
-    boolean email(String email)
-    {
-        Pattern emailRegex = Pattern.compile("^([a-zA-Z0-9]+)([_+.-]{1}[a-z0-9]+)*@([A-Za-z0-9]+).([a-z]{2,4})((.[a-z]{2,3})?)$");
-        Matcher emailMatch = emailRegex.matcher(email);
-	boolean result = emailMatch.matches();
+        boolean result = nameMatch.matches();
         if (result)
             return true;
         else
-            return false;        
+            throw new InvalidInputException("plz enter name with First Letter Capital ");
+
     }
-    boolean password(String password)
+    boolean mobileNumber(String number) throws InvalidInputException
+    {
+        Pattern numberRegex = Pattern.compile("^[0-9]{2}\s[1-9]{1}[0-9]{9}$");
+        Matcher numberMatch = numberRegex.matcher(number);
+        boolean result = numberMatch.matches();
+        if (result)
+            return true;
+        else
+            throw new InvalidInputException("plz enter phone number n ths :91 1234567890: format");
+    }
+    boolean email(String email)throws InvalidInputException
+    {
+        Pattern emailRegex = Pattern.compile("^([a-zA-Z0-9]+)([_+.-]{1}[a-z0-9]+)*@([A-Za-z0-9]+).([a-z]{2,4})((.[a-z]{2,3})?)$");
+        Matcher emailMatch = emailRegex.matcher(email);
+        boolean result = emailMatch.matches();
+        if (result)
+            return true;
+        else
+            throw new InvalidInputException("email should be  like abc.xyz@gmail.com.on with xyz and .in optional ");
+    }
+    boolean password(String password)throws InvalidInputException
     {
         Pattern passwordRegex = Pattern.compile("^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!_%*#?&]{1})[a-zA-Z0-9@$!_%*#?&]{8,}$");
         Matcher passwordMatch = passwordRegex.matcher(password);
-        return passwordMatch.matches();
+        boolean result = passwordMatch.matches();
+        if (result)
+            return true;
+        else
+            throw new InvalidInputException("pass word should have at minimum one uppercase and one numeric and exctly one special char  ");
     }
 }
--- src/userRegistration/UserRegistrationGradleTest.java
+++ src/userRegistration/UserRegistrationGradleTest.java
@@ -6,34 +6,97 @@ import org.junit.Test;
 public class UserRegistrationGradleTest {
     @Test
     public void testFirstName() {
-        UserRegistrationGradle add = new UserRegistrationGradle();
-        boolean output = add.checkName("Vinay");
-        Assert.assertTrue(output);
+        try {
+            try {
+                UserRegistrationGradle add = new UserRegistrationGradle();
+                boolean output = add.checkName(null);
+                Assert.assertTrue(output);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        } catch (InvalidInputException e) {
+            e.printStackTrace();
+        }
     }
+
     @Test
-    public void testLastName() {
-        UserRegistrationGradle lastName = new UserRegistrationGradle();
-        boolean output = lastName.checkName("Rajahs");
-        Assert.assertTrue(output);
+    public void testLastName()
+    {
+        try {
+            try {
+                UserRegistrationGradle lastName = new UserRegistrationGradle();
+                boolean output = lastName.checkName("Rajahs");
+                Assert.assertTrue(output);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        } catch (InvalidInputException e) {
+            e.printStackTrace();
+        }
+
     }
+
     @Test
-    public void testEmailId() {
-        UserRegistrationGradle newEmail = new UserRegistrationGradle();
-        boolean output = newEmail.email("abc1234@gmail.com");
-        Assert.assertTrue(output);
+    public void testEmailId()
+    {
+      try
+       {    try
+            {
+                UserRegistrationGradle newEmail = new UserRegistrationGradle();
+                boolean output = newEmail.email("abc1234@gmail.com");
+                Assert.assertTrue(output);
+            }
+            catch (NullPointerException e)
+             {
+                e.printStackTrace();
+             }
+       }
+       catch(InvalidInputException e)
+       {
+           e.printStackTrace();
+       }
     }
 
 	@Test
-	public void testMobile() {
-        UserRegistrationGradle phoneNumber = new UserRegistrationGradle();
-		boolean output = phoneNumber.mobileNumber("91 1204356789");
-		Assert.assertTrue(output);
+	public void testMobile()
+	{
+	    try
+       {    try
+            {
+                UserRegistrationGradle phoneNumber = new UserRegistrationGradle();
+                boolean output = phoneNumber.mobileNumber("91 1204356789");
+                Assert.assertTrue(output);
+            }
+             catch (NullPointerException e)
+             {
+                e.printStackTrace();
+             }
+       }
+       catch(InvalidInputException e)
+       {
+           e.printStackTrace();
+       }
 	}
 
     @Test
-    public void testPassword() {
-        UserRegistrationGradle passWord = new UserRegistrationGradle();
-       boolean output = passWord.password("1A2xC@1asB");
-       Assert. assertTrue(output);
+    public void testPassword()
+    {
+       try
+       {    try
+            {
+                UserRegistrationGradle passWord = new UserRegistrationGradle();
+               boolean output = passWord.password("1A2xC@1asB");
+               Assert. assertTrue(output);
+            }
+             catch (NullPointerException e)
+             {
+                e.printStackTrace();
+             }
+       }
+       catch(InvalidInputException e)
+       {
+           e.printStackTrace();
+       }
     }
+
 }
